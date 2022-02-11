0adde3ddd8f233b5364713574d2f400ee7b4569a
Andrea Julca Batch <andrea.julca@gmail.com>
2022-02-11 08:39:37 -0500

    works with optomancy pushed to optomancy directory. nginx config file added because this is the version where it actually connected

--- build/index.js
+++ build/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+//import * as socketio from "socket.io";
 const path = require("path");
 var cors = require('cors');
 const app = express();
@@ -9,7 +10,10 @@ app.set("port", process.env.PORT || 8440);
 let http = require("http").Server(app);
 // set up socket.io and bind it to our
 // http server.
-let io = require("socket.io")(http);
+let io = require('socket.io')(http, {
+    pingTimeout: 60000,
+    credentials: true
+});
 /*, {
   cors: {
     origin: "https://optomancy.com",
@@ -17,8 +21,13 @@ let io = require("socket.io")(http);
     credentials: true
   }
 }*/
+app.use(express.static('./.well-known')); //Serves resources from public folder
+app.use('/.well-known', express.static(__dirname + '/.well-known'));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./client/index.html"));
+});
+app.get("/test", (req, res) => {
+    res.sendFile("./landing.html", { root: 'src' });
 });
 // whenever a user connects on port 3000 via
 // a websocket, log that a user has connected
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import * as socketio from "socket.io";
+//import * as socketio from "socket.io";
 import * as path from "path";
 var cors = require('cors')
 
@@ -10,7 +10,10 @@ app.set("port", process.env.PORT || 8440);
 let http = require("http").Server(app);
 // set up socket.io and bind it to our
 // http server.
-let io = require("socket.io")(http);
+let io = require('socket.io')(http, {
+  pingTimeout: 60000,
+  credentials: true
+});
 /*, {
   cors: {
     origin: "https://optomancy.com",
@@ -19,8 +22,16 @@ let io = require("socket.io")(http);
   }
 }*/
 
+app.use(express.static('./.well-known')); //Serves resources from public folder
+
+app.use('/.well-known', express.static(__dirname + '/.well-known'))
+
 app.get("/", (req: any, res: any) => {
   res.sendFile(path.resolve("./client/index.html"));
+});
+
+app.get("/test", (req: any, res: any) => {
+  res.sendFile("./landing.html",  {root: 'src'});
 });
 
 // whenever a user connects on port 3000 via
